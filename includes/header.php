b9b82d28c7ab2f1445ee1e0d06658c8af97cb35b
LuizMiguel05 <josehenrique2362@gmail.com>
2021-10-22 11:43:23 +0000

    Nav (2)

--- includes/header.php
+++ includes/header.php
@@ -13,18 +13,21 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="/index.php">Biblioteca</a>
-            <button class="navbar-togger" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent" aria-expanded="false">
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        
                 <span class="navbar-toggler-icon"></span>
+            
             </button>
+            
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="/index.php">Gêneros</a>
+                        <a class="nav-link" aria-current="page" href="/index.php">Gêneros</a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="/list_livros.php">Livros</a>
+                        <a class="nav-link" aria-current="page" href="/list_livros.php">Livros</a>
                     </li>
     
                 </ul>
