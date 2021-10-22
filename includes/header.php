18f70a050f06ab48b17aa197ce3f6a9cdbf4d8a4
LuizMiguel05 <josehenrique2362@gmail.com>
2021-10-22 11:05:50 +0000

    Remoção do rodapé

--- includes/header.php
+++ includes/header.php
@@ -4,22 +4,6 @@
         <meta charset="UTF-8">
         <title>Biblioteca - <?= $_title ?></title>
        
-        <!-- Estilo para usar rodapé na pagina, não tem haver com o código principal do professor -->
-        <style>
-           
-           footer{
-               
-                Font-family: cursive;
-                position: absolute;
-                bottom:0px;
-                Width: 100%;
-                text-align: center; 
-                background-color:slategrey;
-        
-
-           }
-
-        </style>
        
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
