8a9bdfac8cee556b64eb3c024bf5c871ede008c9
LuizMiguel05 <josehenrique2362@gmail.com>
2021-10-22 11:32:31 +0000

    NavBar

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,33 @@
        
        
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    </head>
+    
+</head>
     <body>  
-        <main class="container">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/index.php">Biblioteca</a>
+            <button class="navbar-togger" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-expanded="false">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/index.php">Gêneros</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="/list_livros.php">Livros</a>
+                    </li>
+    
+                </ul>
+            </div>
+    
+        </div>
+
+        </nav>
+    <br />
+    
+    <main class="container">
